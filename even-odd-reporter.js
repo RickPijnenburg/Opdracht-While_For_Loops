01f4bdab8e1f07e9cfd709dc8bf245053eac6961
RickPijnenburg <32143619+RickPijnenburg@users.noreply.github.com>
2021-09-21 11:48:28 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,8 @@
+console.log('Check if number between 1 and 20 is even or odd?');
+for (let z = 1; z <= 20; z++){
+    if( z%2===0){
+        console.log(z + ' is even');
+    } else {
+        console.log(z + ' is odd');
+    }
+};
---
+++ multiplication-tables.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++){
+    console.log(i + ' x 9 = ' + (i*9));
+};
+
+for (let x = 1; x <=10; x++){
+    for (let y = 1; y<= 10; y++){
+        console.log(x + ' x ' + y + ' = ' + (x*y));
+    }
+}
